e70e4e43d129b4ed04052fea6097a195c3496bf7
Nghia <nghiango0409@gmail.com>
2021-10-03 11:19:40 +0700

    Data Structure
    Finish BST Exercise 3/10/2021

--- Data Structure/BST.cpp
+++ Data Structure/BST.cpp
@@ -53,6 +53,77 @@ bool Search(const Node* root, int value)
         return Search(root->right, value);
     }
 }
+int LeftMostChild(const Node* root)
+{
+    while(root->left != NULL)
+    {
+        root = root->left;
+    }
+    return root->data;
+}
+Node* Delete(Node* root, int value)
+{
+    if(root == NULL)
+    {
+        return root;
+    }
+    if(LeftOf(value, root))
+    {
+        root->left = Delete(root->left, value);
+    }
+    else if(RightOf(value, root))
+    {
+        root->right = Delete(root->right, value);
+    }
+    else
+    {
+        if(root->left == NULL)
+        {
+            Node* newNode = root->right;
+            delete(root);
+            return newNode;
+        }
+        if(root->right == NULL)
+        {
+            Node* newNode = root->left;
+            delete(root);
+            return newNode;
+        }
+        root->data = LeftMostChild(root->right);
+        root->right = Delete(root->right, root->data);
+    }
+    return root;
+}
+void PreOrder(Node* root)
+{
+    if(root != NULL)
+    {
+        cout << root->data << " ";
+        PreOrder(root->left);
+        PreOrder(root->right);
+    }
+    cout << endl;
+}
+void InOrder(Node* root)
+{
+    if(root != NULL)
+    {
+        InOrder(root->left);
+        cout << root->data << " ";
+        InOrder(root->right);
+    }
+    cout << endl;
+}
+void PostOrder(Node* root)
+{
+    if(root != NULL)
+    {
+        PostOrder(root->left);
+        PostOrder(root->right);
+        cout << root->data << " ";
+    }
+    cout << endl;
+}
 int main()
 {
     
