38e1e5e0c217601100b1b83e325028a16826624d
Nghia <nghiango0409@gmail.com>
2021-10-20 11:47:56 +0700

    More Exercise in Unit 13
    Finish Exercise 30 20/10/2021

---
+++ Unit13/Bai30.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <math.h>
+#include <map>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
+bool isPrime(int n)
+{
+    if(n < 2)
+    {
+        return false;
+    }
+    else
+    {
+        for(int i = 2; i <= sqrt(n); i++)
+        {
+            if(n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+bool ContainFirstOdd(int n)
+{
+    int temp;
+    while(n != 0)
+    {
+        temp = n % 10;
+        n /= 10;
+    }
+    if(temp % 2 == 1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+int main()
+{
+    srand((int) time(0));
+    int cnt;
+    int max = 30, min = 1;
+    do
+    {
+        cin >> cnt;
+    } while (cnt > (max + min - 1));
+    int* a = new int[cnt];
+    map<int, bool> vis;
+    for(int i = 0; i < cnt; i++)
+    {
+        do
+        {
+            a[i] = min + rand() % (max + min - 1);
+        } while (vis.find(a[i]) != vis.end());
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    for(int i = 0; i < cnt; i++)
+    {
+        if(isPrime(a[i]) && ContainFirstOdd(a[i]))
+        {
+            cout << a[i] << " ";
+        }
+    }
+    cout << endl;
+}
