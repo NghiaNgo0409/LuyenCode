40739f09f13ae1cc7ee82136e8fbbba1b0cf5d04
Nghia <nghiango0409@gmail.com>
2021-10-24 10:14:57 +0700

    More Exercise in Unit 13
    Fixing Exercise 43 24/10/2021

--- Unit13/Bai43.cpp
+++ Unit13/Bai43.cpp
@@ -1,76 +1,21 @@
-
-#include <stdio.h>
- 
-// Kiểm tra theo kiểu bài toán thuận
-bool IsASC1(int a[], int n)
+#include <iostream>
+using namespace std;
+bool isIncreasing(int a[], int n)
 {
-    int dem = 0;
-    for (int i = 0; i < n - 1; i++)
+    for(int i = 0; i < n - 1; i++)
     {
-        if (a[i + 1] >= a[i])
-            dem++;
-    }
-    if (dem == n - 1)
-        return true;
-    else
-        return false;
-}
- 
-// Kiểm tra theo kiểu bài toán ngịch
-bool IsASC2(int a[], int n)
-{
-    for (int i = 0; i < n - 1; i++)
-    {
-        if (a[i] > a[i + 1])
-            return false;
+        for(int j = i + 1; j < n; j++)
+        {
+            if(a[i] > a[j])
+            {
+                return false;
+            }
+        }
     }
     return true;
 }
- 
-// Kiểm tra theo kiểu bài toán thuận
-bool IsDESC1(int a[], int n)
-{
-    int dem = 0;
-    for (int i = 0; i < n - 1; i++)
-    {
-        if (a[i + 1] <= a[i])
-            dem++;
-    }
-    if (dem == n - 1)
-        return true;
-    else
-        return false;
-}
- 
-// Kiểm tra theo kiểu bài toán ngịch
-bool IsDESC2(int a[], int n)
-{
-    for (int i = 0; i < n - 1; i++)
-    {
-        if (a[i] < a[i + 1])
-            return false;
-    }
-    return true;
-}
- 
-void check(int a[], int n){
-    if(IsASC1(a, n)){
-        printf("\nDay so tang dan!");
-    }else if(IsDESC1(a, n)){
-        printf("\nDay so giam dan!");
-    }else{
-        printf("\nDay so khong tang khong giam!");
-    }
-}
- 
 int main()
 {
-    int a1[] = {1, 3, 5, 7, 7};
-    int a2[] = {4, 3, 2, 1, 1, 1};
-    int a3[] = {1, 3, 2, 4, 5, 6};
- 
-    check(a1, sizeof(a1)/ sizeof(int));
-    check(a2, sizeof(a2)/ sizeof(int));
-    check(a3, sizeof(a3)/ sizeof(int));
+    int a[5] = {1,2,3,4,5};
+    cout << isIncreasing(a, 5);
 }
- 
