78061b034aa1f0ea6252cf4b0bba987780a233f6
Nghia <nghiango0409@gmail.com>
2021-10-26 11:22:27 +0700

    More Exercise in Unit 13
    Finish Exercise 59 26/10/2021

---
+++ Unit13/Bai59.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+int main()
+{
+    cout << Fibonacci(5);
+}
