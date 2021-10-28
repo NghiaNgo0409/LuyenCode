2e7c5f3670ddda2e7c9bb4544dc4bc8019c5ddae
Nghia <nghiango0409@gmail.com>
2021-10-28 15:06:06 +0700

    More LuyenCode Website
    Finish PTIT051 28/10/2021

---
+++ LuyenCodeWeb/PTIT051.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int date, month, year;
+    cin >> date >> month >> year;
+    cout << "users setClock \\" << month << "\\" << date << "\\" << year;
+}
