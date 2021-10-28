296bb02fa344509f990efafb8ebc58b047a990f6
Nghia <nghiango0409@gmail.com>
2021-10-28 15:25:17 +0700

    More LuyenCode Website
    Finish CGAME 28/10/2021

---
+++ LuyenCodeWeb/AVG3NUM.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, x, c;
+    cin >> a >> b >> x;
+    c = x * 3 - (a + b);
+    cout << c;
+}
---
+++ LuyenCodeWeb/CGAME.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long a, b;
+    cin >> a >> b;
+    if(a > b)
+    {
+        cout << 1;
+    }
+    else if (a < b)
+    {
+        cout << 0;
+    }
+}
