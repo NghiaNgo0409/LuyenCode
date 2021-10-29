b512e5d94d5e8fa9da617ab044978f9133e706b0
Nghia <nghiango0409@gmail.com>
2021-10-29 11:35:30 +0700

    More LuyenCode Website
    Finish ABC042_A 29/10/2021

---
+++ LuyenCodeWeb/ABC042_A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a + b + c == 17)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+}
